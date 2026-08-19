f9f5eb8cce0554f6f69ab3a73dd320c7737a29e2
suhana6515 <suhana.shrestha.6515@gmail.com>
2026-08-19 07:20:46 +0545

    Changes

--- backend/controllers/petController.js
+++ backend/controllers/petController.js
@@ -5,16 +5,19 @@ const pet = require("../models/pet");
  * @access  Public
  */
 
-const createPet = async (req,res) => {
-    try{
-        const {name, type, background} = res.body;
-        if(!name || !type ){
-            return res.status(400).json({message:"Name and Pet Type are required"});
-        }
-        const newPet = await pet.create({
-            
-        })
-    }catch{
-
+const createPet = async (req, res) => {
+  try {
+    const { name, type, background } = res.body;
+    if (!name || !type) {
+      return res
+        .status(400)
+        .json({ message: "Name and Pet Type are required" });
     }
-}
+    const newPet = await pet.create({
+      id: req.user.id,
+      name,
+      type,
+      background,
+    });
+  } catch {}
+};
